646121f4ccf06367961192fa5fca5cf338f97fbc
a.c.max <geey11u@126.com>
2018-01-20 17:43:53 +0800

    work well.
    Signed-off-by: a.c.max <geey11u@126.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <thread>
+#include "videoCamera.h"
+
+void record(videoCamera *camera){
+  
+  std::ofstream lfile("rdbuf.txt");
+  std::streambuf *x = std::cout.rdbuf(lfile.rdbuf());  
+  std::streambuf *r = std::cerr.rdbuf(lfile.rdbuf());
+  camera->encode();
+  std::cout.rdbuf(x);
+  std::cerr.rdbuf(r);
+}
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+  
+  videoCamera camera;  
+  
+  char ter;
+  std::thread t(record, &camera);
+  t.detach();
+  
+  std::cin>>ter;
+  camera.terminal();
+//   camera.encode();
+  
+  return 0;
 }
